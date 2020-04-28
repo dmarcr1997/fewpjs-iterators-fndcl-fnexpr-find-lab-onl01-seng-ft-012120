dc95cb9300510522700b05a16f3f7d5e5da69a03
Damon Marc Rocha <dmarcr1997@gmail.com>
2020-04-28 14:09:22 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(teams){
+  let win = teams.find( function(t){ return t.result === "W"});
+  
+  if (win !== undefined){
+    return win.year;
+  }
+  else{
+    return win;
+  }
+}
